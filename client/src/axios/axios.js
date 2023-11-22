960d8fdb514ec22a3312e07db5018824e86e83b8
Ritik Sharma <sharmaritik2002@gmail.com>
2023-11-22 21:23:09 +0530

    initial commit

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const auth_axios = axios.create(
     {
-        baseURL: 'https://chat-app-server-mfph.onrender.com',
+        baseURL: 'https://chat-app-server-mfph.onrender.com/api/auth',
         timeout: 5000        
     }
 )
